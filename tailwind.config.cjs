6bba0f383b7a620da5872b7db155bf1de3815b5f
nil <xetaxe@gmail.com>
2023-01-23 11:20:04 +0100

    Basic project config setup (vite, react, tailwind, postcss) ready

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,tsx,ts}"],
+  theme: {
+    screens: {
+      md: '900px',
+      lg: '1160px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
